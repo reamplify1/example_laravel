7373218390218d6eafc9e3908df6061cdad7a0ee
reamplify <126955026+reamplify1@users.noreply.github.com>
2025-02-01 02:37:41 +0500

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+
+use Illuminate\Support\Facades\DB;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::preventLazyLoading();
+        // Включаем поддержку внешних ключей для SQLite
+        if (env('DB_CONNECTION') === 'sqlite') {
+            DB::statement('PRAGMA foreign_keys = ON;');
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function ()  {
+    $jobs = Job::with('employer')->get();
+    
+    return view('jobs', [
+        'jobs' => $jobs
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id)  {
+
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
